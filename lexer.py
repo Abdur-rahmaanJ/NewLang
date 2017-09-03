3cb63015265104d500c7565845a6f6dc9d133cd2
TedLastname <tedsnookn@gmail.com>
2017-09-03 16:35:28 -0500

    Add files via upload

---
+++ lexer.py
@@ -0,0 +1,57 @@
+def read(text):
+    stack = []
+    with open(text, 'r') as f:
+        for line in f:
+            stack = line.split()
+            diction = {}
+            test(stack, diction)
+            
+def test(stack, variables):
+    if len(stack) > 1:
+        for i in range(len(stack)):
+            if stack[i] == '+':
+                stack[i] = str(int(stack[i-1]) + int(stack[i-2]))
+                del stack[i-1]
+                del stack[i-2]
+                break
+            elif stack[i] == '-':
+                stack[i] = str(int(stack[i-1]) - int(stack[i-2]))
+                del stack[i-1]
+                del stack[i-2]
+                break
+            elif stack[i] == '*':
+                stack[i] = str(int(stack[i-1]) * int(stack[i-2]))
+                del stack[i-1]
+                del stack[i-2]
+                break
+            elif stack[i] == '/':
+                stack[i] = str(int(stack[i-1]) / int(stack[i-2]))
+                del stack[i-1]
+                del stack[i-2]
+                break
+            elif stack[i] == '//':
+                stack[i] = str(int(stack[i-1]) // int(stack[i-2]))
+                del stack[i-1]
+                del stack[i-2]
+                break
+            elif stack[i] == 'print':
+                print(stack[i-1])
+                del stack[i]
+                del stack[i-1]
+                break
+            elif stack[i] == '^':
+                stack[i] = str(int(stack[i-1]) ** int(stack[i-2]))
+                del stack[i-1]
+                del stack[i-2]
+                break
+            elif stack[i] == '=':
+                variables[stack[i-1]] = stack[i-2]
+                del stack[i]
+                del stack[i-1]
+                del stack[i-2]
+                break
+            elif stack[i] in variables:
+                stack[i] = variables[stack[i]]
+        test(stack, variables)
+
+read("test.txt")
